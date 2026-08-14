bdc7cf28a6e852f30f4bac7c41ef8885976052bf
-v <kool5567@users.noreply.github.com>
2026-08-14 11:16:47 +0300

    Remove stray dev helper file

--- db-check.js
+++
@@ -1,9 +0,0 @@
-const fs = require("fs");
-const { PrismaClient } = require("@prisma/client");
-const l = fs.readFileSync(process.cwd() + "/.env.local", "utf8").split(/\r?\n/).filter((x) => x.startsWith("DATABASE_URL=")).pop();
-const url = l.slice(l.indexOf("=") + 1).trim().replace(/^"|"$/g, "");
-const p = new PrismaClient({ datasources: { db: { url } } });
-p.user
-  .count()
-  .then((c) => { console.log("DB OK, users:", c); return p.$disconnect(); })
-  .catch((e) => { console.log("DB FAIL:", e.message.split("\n")[0]); process.exit(1); });
